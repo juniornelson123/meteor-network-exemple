006dea901df219683e34e8b05f815872a0ae6ad1
juniornelson123 <junior123nelson@gmail.com>
2017-04-12 17:45:23 -0300

    corrigindo follow e unfollow

--- client/routes/friendship.js
+++ client/routes/friendship.js
@@ -1,12 +1,12 @@
 Router.route("/user/:_id/follow", function(){
 	var _id = this.params._id
-	Friendships.follow(_id)
+	Meteor.call("followUser",	_id);
 	this.redirect("/user/"+_id)
 }, {name: "user.follow"})
 
 Router.route("/user/:_id/unfollow",	function() {
 		var	_id	=	this.params._id
-		Friendships.unfollow(_id)
+		Meteor.call("unfollowUser",	_id);
 		this.redirect("/user/"+_id)
 		
 },	{	name:	"user.unfollow"	});	
